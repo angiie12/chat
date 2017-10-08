0ac05fcbbed0170855c608b2b38c72b463dd115b
Eryk <rcksorto@yahoo.com>
2017-10-08 15:14:48 -0700

    Convert chat server function to class

--- chatserver.py
+++ chatserver.py
@@ -1,63 +1,60 @@
 import select
 import socket
-import sys
+import threading
 
 
-HOST = ''
-PORT = 2048
-CLIENTS = []
+class ChatServer(threading.Thread):
 
+    def __init__(self, port):
+        threading.Thread.__init__(self)
 
-def broadcast(server, omitted_client, message):
-    for client in CLIENTS:
-        if client != server and client != omitted_client:
-            try:
-                client.send(message)
-            except socket.error:
-                client.close()
-                if client in CLIENTS:
-                    CLIENTS.remove(client)
+        self.clients = []
+        self.port = port
+        self.server = None
 
-
-def chat_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((HOST, PORT))
-    server.listen(10)
-
-    CLIENTS.append(server)
-
-    print 'Chat server started on port %d' % PORT
-
-    while True:
-        read_connections, _, _ = select.select(CLIENTS, [], [], 0)
-
-        for client in read_connections:
-            if client == server:
-                new_client, address = server.accept()
-                CLIENTS.append(new_client)
-                print 'Client (%s, %s) connected' % address
-                broadcast(server, new_client,
-                          '[%s:%s] entered our chatting room\n' % address)
-            else:
+    def broadcast(self, omitted_client, message):
+        for client in self.clients:
+            if client != self.server and client != omitted_client:
                 try:
-                    data = client.recv(4096)
-
-                    if data:
-                        broadcast(server, client,
-                                  '\r[%s] %s' % (str(client.getpeername()), data))
-                    else:
-                        if client in CLIENTS:
-                            CLIENTS.remove(client)
-                        broadcast(server, client,
-                                  'Client (%s, %s) is offline\n' % address)
+                    client.send(message)
                 except socket.error:
-                    broadcast(server, client,
-                              'Client (%s, %s) is offline\n' % address)
-                    continue
+                    client.close()
+                    if client in self.clients:
+                        self.clients.remove(client)
 
-    server.close()
+    def run(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind(('', self.port))
+        self.server.listen(10)
 
+        self.clients.append(self.server)
 
-if __name__ == '__main__':
-    sys.exit(chat_server())
+        print 'Chat server started on port %d' % self.port
+
+        while True:
+            read_connections, _, _ = select.select(self.clients, [], [], 0)
+
+            for client in read_connections:
+                if client == self.server:
+                    new_client, address = self.server.accept()
+                    self.clients.append(new_client)
+
+                    print 'Client (%s, %s) connected' % address
+
+                    self.broadcast(new_client, '[%s:%s] joined\n' % address)
+                else:
+                    try:
+                        data = client.recv(4096)
+
+                        if data:
+                            self.broadcast(client, '\r[%s] %s' % (str(client.getpeername()), data))
+                        else:
+                            if client in self.clients:
+                                self.clients.remove(client)
+                            self.broadcast(client, 'Client (%s, %s) is offline\n' % address)
+                    except socket.error:
+                        self.broadcast(client, 'Client (%s, %s) is offline\n' % address)
+                        continue
+
+        self.server.close()
